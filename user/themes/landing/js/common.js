5382ef470994debb7b9504266c92502c0048d7ae
gooddha <gooddha@ya.ru>
2017-06-19 15:58:24 +0300

    сделал AJAX отправку формы и плавный переход к форме

---
+++ user/themes/landing/js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	$(".order-button").mPageScroll2id();
+
+	$("#order").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+
+});
