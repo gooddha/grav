6163723ee9d1bc78ca87d848949896bd769b53d8
gooddha <gooddha@ya.ru>
2017-07-12 17:37:44 +0300

    Подключил плагин email к ящику monopoly.crimea.@gmail.com

--- mail.php
+++ mail.php
@@ -1,11 +1,19 @@
 <?php
 
-$recepient = "monopoly.crimea@gmail.com";
-$sitename = "Монополия Крым";
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$from = 'monopoly.crimea@gmail.com';
+$to = "monopoly.crimea@gmail.com";
+$title = "Новая заявка с сайта \"Монополия Крым\"";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
+
 $message = "Был оформлен заказ. \nИмя: $name \nТелефон: $phone";
 
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+// mail($to, $title, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+
+$message = $this->grav['Email']->message($title, $message, 'text/html')
+    ->setFrom($from)
+    ->setTo($to);
+
+$sent = $this->grav['Email']->send($message);
